ddbbfd50828a4bab8263d0355ff25102c3a6cbe6
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2019-12-12 09:36:43 +0100

    Add files via upload

--- Tp3/dag.py
+++ Tp3/dag.py
@@ -5,6 +5,7 @@ Created on Tue Dec 10 14:04:24 2019
 @author: 33762
 """
 
+#==============================ETAPE==============================
 class etape:
     """
     -numero
@@ -40,43 +41,80 @@ class etape:
             res.append(tache.etape_suivante)
         return res
     
-    def get_previous_steps(self,noeudAChercher,res=[]):
-        """
-        ne fonctionne pas quand il a plusieurs prédéceceurs
-        """
-        if self.get_next_steps() == [] : #fin de la recursivité
-            return res # ne retourne rien 
-        else :
-            for etape_suivante in self.get_next_steps(): 
-                print ("self :"+str(self)+" suivant : "+str(etape_suivante))
-                if etape_suivante == noeudAChercher:
-                    res.append(self.numero)
-                return etape_suivante.get_previous_steps(noeudAChercher,res)
-                
+    def get_previous_steps(self,noeudAChercher):
+        res = []
+        liste_noeuds = self.parcourir()
+        for noeud in liste_noeuds:
+            if noeudAChercher in noeud.get_next_steps():
+                res.append(noeud)
+        return res
+    
+    
+    def parcourir(self, liste_passage = [], liste_noeuds = []):
+        liste_noeuds = [self]
+        for etape in self.get_next_steps():
+            if not etape in liste_passage:
+                liste_passage.append(etape)
+                liste_noeuds += etape.parcourir(liste_passage)
+        return liste_noeuds
+    
+   
+#==============================TACHE==============================  
 class tache:
     """
     -nom 
     -duree
+    -etape precedente
     -etape suivante
     """
     
-    def __init__(self,nom,duree,etape_suivante=None):
+    def __init__(self,nom,duree,etape_suivante=None,etape_precedente=None):
         """
         constructeur de la classe tache
         """
         self.nom = nom
         self.duree = duree
+        self.etape_precedente = etape_precedente
         self.etape_suivante = etape_suivante
         
     def __str__(self):
         return self.nom
 
+    def set_etape_pre(self,etape_pre):
+        self.etape_precedente = etape_pre
+        
+    def get_name(self):
+        return self.nom
+    
+    def get_duration(self):
+        return self.duree
+    
+    def get_begin_step(self):
+        return self.etape_precedente
+    
+    def get_end_step(self):
+        return self.etape_suivante
+    
+
+#==============================MAIN==============================
+def critique(pert,chemin=[]):
+        chemin.append(pert)
+        for etape in pert.get_next_steps():
+            if not etape == None:
+                if etape.get_au_plus_tard() == etape.get_au_plus_tot():
+                    return critique(etape,chemin)
+        if pert.get_next_steps() == []:
+            return chemin  
+        
+#==============================TESTS==============================
+        
 etape8 = etape('8',220,220) 
 tacheH = tache('H',10,etape8)
 etape7 = etape('7',210,210,[tacheH]) 
 tacheG = tache('G',60,etape7)
-tacheT = tache('T',0,etape7)   
-etape5 = etape('5',50,210,[tacheT]) 
+tacheT = tache('T',0,etape7)
+tacheT2 = tache('T',0,etape7)     
+etape5 = etape('5',50,210,[tacheT2]) 
 etape6 = etape('6',150,150,[tacheG])    
 etape3 = etape('3',150,210,[tacheT])   
 tacheE = tache('E',10,etape5)
@@ -90,4 +128,16 @@ etape1 = etape('1',30,30,[tacheB,tacheD])
 tacheA = tache('A',30,etape1)
 etape0 = etape('0',0,0,[tacheA])
 
-print(etape0.get_previous_steps(etape7))
+
+tacheA.set_etape_pre(etape0)
+tacheB.set_etape_pre(etape1)
+tacheC.set_etape_pre(etape2)
+tacheD.set_etape_pre(etape1)
+tacheE.set_etape_pre(etape4)
+tacheG.set_etape_pre(etape6)
+tacheH.set_etape_pre(etape7)
+tacheT.set_etape_pre(etape3)
+tacheT2.set_etape_pre(etape5)
+
+
+print(critique(etape0))
